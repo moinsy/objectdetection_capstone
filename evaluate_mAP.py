e6fe8b013300586bf0cf6c85209c4ef93d094ca0
moin <moinuddin.syed@justdial.com>
2017-11-27 17:07:59 +0530

    edit mAP

--- evaluate_mAP.py
+++ evaluate_mAP.py
@@ -26,12 +26,12 @@ def bb_intersection_over_union(boxA, boxB):
     return iou
 
 
-with open('test_images_result.json') as f:
+with open('data/test_images_result.json') as f:
     test_j = json.load(f)
 
-test_df = pd.read_csv('product_bbox.csv')
+test_df = pd.read_csv('../data/train/product_bbox.csv')
 
-pdts = pd.read_csv('/home/justdial/PycharmProjects/objectdetection_oid/data/products.csv')
+pdts = pd.read_csv('data/products.csv')
 
 
 def get_label(id):
@@ -51,35 +51,37 @@ def get_predicted_box(img):
 
     return pred_res
 
-def getmAP():
-    res = {}
-    tp,fp,fn,precision,recall,p_recall,ap = 0,0,0,0,0,0,0
 
-    u_imgs = test_df.ImageID.unique().tolist()
+res = {}
+tp,fp,fn,precision,recall,p_recall,ap = 0,0,0,0,0,0,0
 
-    for i,img in enumerate(u_imgs):
-    #     tp,fp,fn,precision,recall = 0,0,0,0,0
+u_imgs = test_df.ImageID.unique().tolist()
+
+for i,img in enumerate(u_imgs):
+
         annon = test_df[test_df.ImageID.isin([img])]
         pred_res = get_predicted_box(img)
         if not pred_res:
             continue
-    #     print ("img:{}".format(img))
+
         for row in annon.iterrows():
             label = row[1]['LabelName']
             box = [row[1]['YMin'],row[1]['XMin'],row[1]['YMax'],row[1]['XMax']]
             label_check = False
+
             for key in pred_res:
+
                 boxp = pred_res[key]
                 iou = bb_intersection_over_union(box,boxp)
-    #             print ("label:{},{}, iou:{}".format(label,key,iou))
+
                 if iou > 0.5:
                     if label == key:
-                        tp+=1
+                        tp += 1
                     else:
-                        fp+=1
+                        fp +=1
                 label_check = True
             if not label_check:
-                fn+=1
+                fn += 1
 
         if tp+fp == 0 and tp+fn!=0:
             precision = 0
@@ -93,14 +95,11 @@ def getmAP():
             precision = float(tp)/(tp+fp)
             recall = float(tp)/(tp+fn)
 
-    #     print ("tp:{}, fp:{}, fn:{}".format(tp,fp,fn))
-
 
         rate_recall = recall-p_recall
-    #     print ('r_r:{}'.format(rate_recall))
 
         ap += float(precision) * float(rate_recall)
         mAP = float(ap)/(i+1)
-    #     res[img] = {'tp':tp, 'fp':fp, 'fn':fn, 'precision':precision, 'recall':recall, 'ap':ap, 'mAP':mAP}
-    #     print res[img]
-    print ('The mean Average Precision (mAP) for test images is : {}'.format(mAP))
+
+print ('The mean Average Precision (mAP) for test images is : {}'.format(mAP))
+
